4780f93af2ddf9860d8121ba3b655c6f7c0fca87
Kanzada <abdykadyrkanzada222@gmail.com>
2023-10-11 18:19:17 +0600

    javaUI

---
+++ UI/src/test/java/ButtonsPageTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class ButtonsPageTest extends BaseTest {
+    @Test
+    void test(){
+        driver.get("https://demoqa.com/buttons");
+        buttonsPage.makeDoubleClick().makeRightClick().makeSimpleClick().testButtons();
+    }
+}
---
+++ UI/src/test/java/LinksPageTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class LinksPageTest extends BaseTest {
+    @Test
+    void testLinksPage(){
+        driver.get("https://demoqa.com/links");
+        linksPage
+                .clickToHomeLink()
+                .clickToDynamicLink()
+                .clickToCreateResponseBtn()
+                .clickToNoContentResponseBtn()
+                .clickToMovedResponseBtn()
+                .clickToBadRequestBtn()
+                .clickToUnauthorizedBtn()
+                .clickToForbiddenBtn()
+                .clickToInvalidBtn()
+                .test();
+    }
+}
